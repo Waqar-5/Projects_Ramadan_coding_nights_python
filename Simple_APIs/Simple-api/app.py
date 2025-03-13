65e60c75b83af7d2f18f47257e7a318de0a0f793
Waqar-5 <161034030+Waqar-5@users.noreply.github.com>
2025-03-13 13:15:14 +0500

    main.py

--- Simple_APIs/Simple-api/app.py
+++ Simple_APIs/Simple-api/app.py
@@ -41,6 +41,13 @@ money_quotes = [
 ]
 
 
+@app.get("/")
+def read_root():
+    return {
+        "message": "Hello World, Go to /side_hustles or /money_quotes to get a random side hustle or money quote"
+    }
+
+
 # Decorators
 
 @app.get("/side_hustles")
